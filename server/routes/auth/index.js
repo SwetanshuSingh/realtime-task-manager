cc830a97e50c8cb546b65068d73298f05711f5a7
Swetanshu <swetanshusingh17@gmail.com>
2024-03-12 19:14:46 +0530

    Add : signup api route

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import UserSchema from "../../schema/User.js";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     const { email, username, password } = req.body;
 
@@ -14,11 +17,36 @@ router.post("/signup", (req, res) => {
       });
     }
 
-    
-
-    return res.json({
-      message: "Sign Up Route",
+    const isExisitingUser = await prisma.user.findFirst({
+      where: {
+        email: email,
+        username: username,
+      },
     });
+
+    if (isExisitingUser !== null) {
+      return res.status(403).json({
+        message: "User already Exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: email,
+        username: username,
+        password: hashedPassword,
+      },
+    });
+
+    console.log(newUser);
+
+    if (newUser) {
+      return res.json({
+        message: "User created successfully",
+      });
+    }
   } catch (error) {
     return res.status(500).json({
       Error: "Internal Server Error",
