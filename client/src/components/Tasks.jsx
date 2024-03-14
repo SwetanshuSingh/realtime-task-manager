63398769f7abc71b31e3de8bbad95adaa8ce5bf5
Swetanshu <swetanshusingh17@gmail.com>
2024-03-14 15:02:03 +0530

    Update : styles for tasks card

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -1,22 +1,34 @@
+import { useContext, useEffect } from "react";
 import TaskCard from "./ui/TaskCard";
+import { AuthContext } from "../context/AuthContextProvider";
+
+const getUserTasks = async ( token ) => {
+  const response = await fetch("/api/tasks/all", {
+    headers : {
+      'token' : token
+    }
+  });
+
+  const result = await response.json();
+  console.log(result.data);
+}
 
 export const Tasks = () => {
+
+  const { auth } = useContext(AuthContext);
+
+  useEffect(() => {
+    getUserTasks(auth.token);
+  }, [])
+
   return (
     <section className=" w-full h-full flex flex-col gap-5 bg-[#212121] rounded-xl border-2 border-[#f9f9f9] border-opacity-5 px-8 py-6">
-      <h2 className="font-semibold text-white font-mono text-3xl relative">
+      <h2 className="font-medium text-white text-3xl relative">
         All Tasks
         <span className="absolute top-9 left-0 w-16 h-1 bg-green-500 rounded-full"></span>
       </h2>
 
-      <div className="all-tasks w-full h-full flex flex-row items-center gap-5 flex-wrap overflow-y-scroll">
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
-        <TaskCard />
+      <div className="all-tasks w-full h-full flex flex-row items-justify gap-5 flex-wrap overflow-y-scroll">
         <TaskCard />
       </div>
     </section>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Tasks from "../components/Tasks";
 
 const HomePage = () => {
   return (
-    <main className="w-full h-[100vh] bg-[#181818] p-12 flex justify-between gap-10">
+    <main className="w-full h-[100vh] bg-[#181818] p-12 flex justify-between gap-10 font-0">
       <Sidebar />
       <Tasks />
     </main>
