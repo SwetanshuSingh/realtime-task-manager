1889fc6ef625c48d1488925c5a8ff3461f8cdca1
Swetanshu <swetanshusingh17@gmail.com>
2024-03-14 14:11:41 +0530

    Add : get all tasks and delete tasks endpoint

--- server/routes/tasks/index.js
+++ server/routes/tasks/index.js
@@ -1,9 +1,38 @@
-import { Router } from "express";
+import { Router, json } from "express";
 import authMiddleware from "../../middleware/authMiddleware.js";
 import { PrismaClient } from "@prisma/client";
 
 const router = Router();
 const prisma = new PrismaClient();
+
+router.get("/all", authMiddleware, async (req, res) => {
+  try {
+    const { username } = res;
+
+    const userDetails = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+      include: {
+        tasks: true,
+      },
+    });
+
+    if (userDetails.tasks === null) {
+      return res.status(403).json({
+        message: "You have no tasks",
+      });
+    }
+
+    return res.status(200).json({
+      data: userDetails.tasks,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+});
 
 router.post("/create", authMiddleware, async (req, res) => {
   try {
@@ -37,9 +66,40 @@ router.post("/create", authMiddleware, async (req, res) => {
       },
     });
 
-    res.status(200).json({
+    return res.status(200).json({
       message: "Task created Succesfully",
       data: newTask,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+});
+
+router.post("/delete", authMiddleware, async (req, res) => {
+  try {
+    const { taskId } = req.body;
+    const { username } = res;
+
+    const userDetails = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    const deletedTask = await prisma.task.delete({
+      where: {
+        id: taskId,
+        userId: userDetails.id,
+      },
+    });
+
+    console.log(deletedTask);
+
+    return res.status(200).json({
+      message: "Task Deleted Successfully",
     });
   } catch (error) {
     console.log(error);
