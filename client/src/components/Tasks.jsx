a96fe3b658a954f73a012c3d4359f3de7f3ba195
Swetanshu <swetanshusingh17@gmail.com>
2024-03-14 12:42:48 +0530

    Add : all tasks page and task card

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -1,3 +1,5 @@
+import TaskCard from "./ui/TaskCard";
+
 export const Tasks = () => {
   return (
     <section className=" w-full h-full flex flex-col gap-5 bg-[#212121] rounded-xl border-2 border-[#f9f9f9] border-opacity-5 px-8 py-6">
@@ -6,8 +8,16 @@ export const Tasks = () => {
         <span className="absolute top-9 left-0 w-16 h-1 bg-green-500 rounded-full"></span>
       </h2>
 
-      <div className="all-tasks w-full h-full">
-
+      <div className="all-tasks w-full h-full flex flex-row items-center gap-5 flex-wrap overflow-y-scroll">
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
       </div>
     </section>
   )
---
+++ client/src/components/ui/TaskCard.jsx
@@ -0,0 +1,9 @@
+const TaskCard = () => {
+  return (
+    <div className="w-[320px] h-[220px] rounded-md border-2 bg-[#f9f9f9] bg-opacity-5 border-[#f9f9f9] border-opacity-5">
+
+    </div>
+  )
+}
+
+export default TaskCard;
