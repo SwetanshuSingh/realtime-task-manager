556a7af9cb948f8da5aa6d947fc21439b375ff1e
Swetanshu <swetanshusingh17@gmail.com>
2024-03-16 14:52:30 +0530

    Add : build scripts

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import authRouter from "./routes/auth/index.js";
 import taskRouter from "./routes/tasks/index.js"
@@ -7,9 +8,17 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/tasks", taskRouter)
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));  
+})
 
 app.listen(PORT, () => {
   console.log("Server Running");
