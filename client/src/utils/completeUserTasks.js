b9450b500c664758c93e3acdc741a64db9f09f4f
Swetanshu <swetanshusingh17@gmail.com>
2024-03-16 10:08:52 +0530

    Fix : minor for deleting tasks

--- client/src/utils/completeUserTasks.js
+++ client/src/utils/completeUserTasks.js
@@ -2,21 +2,20 @@ import toast from "react-hot-toast";
 
 const completeTask = async (token, data, setIsCompleted) => {
   const response = await fetch("/api/tasks/update/status", {
-    method : "POST",
-    headers : {
-      'Content-Type' : 'application/json',
-      'token' : token
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      token: token,
     },
-    body : JSON.stringify({taskId : data.id, isCompleted : !data.completed})
-  })
+    body: JSON.stringify({ taskId: data.id, isCompleted: !data.completed }),
+  });
 
   const result = await response.json();
-  if(response.status === 200){
+  if (response.status === 200) {
     setIsCompleted(result.data.completed);
+  } else {
+    toast.error(result.error);
   }
-  else{
-    toast.error(result.error)
-  }
-}
+};
 
 export default completeTask;
--- client/src/utils/deleteUserTasks.js
+++ client/src/utils/deleteUserTasks.js
@@ -1,16 +1,22 @@
+import toast from "react-hot-toast";
+
 const deleteTask = async (token, data, setTasks) => {
   const response = await fetch("/api/tasks/delete", {
-    method : "GET", 
-    headers : {
-      "token" : token,
-      "taskid" : data.id
-    }
+    method: "GET",
+    headers: {
+      token: token,
+      taskid: data.id,
+    },
   });
-  
+
   const result = await response.json();
-  setTasks((prev) => {
-    return prev.filter((task) => task.id !== result.data.id)
-  })
-}
+  if (response.status === 200) {
+    setTasks((prev) => {
+      return prev.filter((task) => task.id !== result.data.id);
+    });
+  } else {
+    toast.error(result.error);
+  }
+};
 
 export default deleteTask;
